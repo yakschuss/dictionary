8076c3dd9be99d98e7fff388ddae46a8b6026da7
Jack Schuss <jackschuss@gmail.com>
2017-06-15 07:54:09 -0400

    Coding Gnome, Initial Commit

---
+++ lib/dictionary.ex
@@ -0,0 +1,12 @@
+defmodule Dictionary do
+  def word_list do
+    "assets/words.txt" 
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def random_word do
+    word_list()
+    |> Enum.random
+  end
+end
