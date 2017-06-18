b46100d3b8a051825f9dd95a49da26ff891a07b1
Jack Schuss <jackschuss@gmail.com>
2017-06-18 10:17:26 -0400

    winning move

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -7,8 +7,14 @@ defmodule Hangman.Game do
     used: MapSet.new(),
   )
 
+  def new_game(word) do
+    %Hangman.Game{
+      letters: word |> String.codepoints
+    }
+  end
+
   def new_game() do
-    %Hangman.Game{ letters: letters_to_be_guessed() }
+    new_game(Dictionary.random_word)
   end
 
   def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
@@ -24,16 +30,27 @@ defmodule Hangman.Game do
     Map.put(game, :game_state, :already_used)
   end
 
-  def accept_move(game, guess, _already_guessed = false) do
+  def accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+
+  def score_guess(game, _good_guess = true) do
+    new_state = MapSet.new(game.letters)
+                |> MapSet.subset?(game.used)
+                |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score_guess(game, _bad_guess) do
+    game
+  end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
 
   defp tally(game) do
     123
-  end
-
-  defp letters_to_be_guessed do
-    Dictionary.random_word  |> String.codepoints
   end
 end
 
